a67207dc3e0c51b66b541c118a4f2765d0630969
tomaraquedepratrocar <josecafran.militao1@gmail.com>
2022-05-13 21:55:59 -0300

    adicionadas anotações e exercícios referentes a introdução ao ecossistema e documentação java

---
+++ JAVA e SpringBoot/introdução ao ecossistema e documentação java/exercícios/src/com/dio/MyFirstProgram.java
@@ -0,0 +1,11 @@
+package com.dio;
+
+import com.dio.base.Order;
+
+public class MyFirstProgram {
+    
+    public static void main(String[] args) {
+        Order order = new Order("1234");
+        System.out.println(order);
+    }
+}
