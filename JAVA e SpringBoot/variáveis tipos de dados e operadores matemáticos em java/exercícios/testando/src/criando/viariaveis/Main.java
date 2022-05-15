027b9a346d59b43d9f1c8663479464f10c94d5fc
tomaraquedepratrocar <josecafran.militao1@gmail.com>
2022-05-15 00:12:09 -0300

    adicionados arquivos de variáveis java

---
+++ JAVA e SpringBoot/variáveis tipos de dados e operadores matemáticos em java/exercícios/testando/src/criando/viariaveis/Main.java
@@ -0,0 +1,27 @@
+package criando.viariaveis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 1;
+        final byte A = 1;
+        short b = 2;
+        final short B = 2;
+        int c = 3;
+        final int C = 3;
+        long d = 4;
+        final long D = 4;
+        float e = 0.4f;
+        final float E = 0.4f;
+        double f = 0.4;
+        final double F = 0.4;
+        char g = 'a';
+        final char G = 'a';
+        String h = "ab";
+        final String H = "ab";
+        boolean i = false;
+        final boolean I = false;
+        System.out.println(a + A + b + B + c +C +d +D + e + E + f +F + g + G + h + H + i + I);
+    }
+
+}
